b572964ef988b2d53274f55235f499e15f701c3f
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-04 15:28:06 +0100

    Product model is done

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+configDotenv();
 const app = express();
-
-app.listen(3000 , () => {
-    console.log("Server is running on port 3000");
-})
 
 app.get("/" , (req , res) => {
     res.status(200).json({
         name : "Amir Dhahri",
         age : 20
     })
+})
+
+mongoose.connect(process.env.MONGO_URL_CONNECTION)
+.then(() => {
+    console.log("Connected to database!");
+    app.listen(3000 , () => {
+        console.log("Server is running on port 3000");
+    })
+})
+.catch(() => {
+    console.log("Connection Failed !");
 })
