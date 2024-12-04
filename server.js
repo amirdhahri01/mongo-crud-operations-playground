abf215d32119a3b6a9bb68df439f1b2783871051
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-04 15:56:26 +0100

    An update

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
+import asyncHandler from "express-async-handler"
 import { configDotenv } from "dotenv";
 import Product from "./models/product.model.js";
 configDotenv();
 const app = express();
 app.use(express.json());
-app.get("/" , (req , res) => {
-    res.status(200).json({
-        name : "Amir Dhahri",
-        age : 20
-    })
+const ProductRoutes = express.Router();
+ProductRoutes.post("" , new asyncHandler(async(req , res)=>{
+    const {name , quantity , price , image} = req.body;
+    const product = await Product.create({name , quantity , price , image});
+    res.status(201).json({message : "Product Created Successfully" , data:{product}})
+})).get("" , async (req , res) => {
+    const products = await Product.find();
+    res.status(200).json({message : "Products Fetched Successfully" , data:{products}})
 })
-app.post("/api/products" ,async(req , res)=>{
-    try{
-        const {name , quantity , price , image} = req.body;
-        const product = await Product.create({name , quantity , price , image});
-        res.status(201).json({message : "Product Created Successfully" , data:{product}})
-    }catch(err){ 
-        res.status(500).json({message : err.message})
-    }
+app.use("/api/products" , ProductRoutes)
+app.use((req , res ,err) => {
+    res.status(500).json({message : err.message})
 })
-
 mongoose.connect(process.env.MONGO_URL_CONNECTION)
 .then(() => {
     console.log("Connected to database!");
